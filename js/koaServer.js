4d769da94f53b3d443e83995e11a13edde830bd2
Murat GOKDUMAN <drmuratgokduman@gmail.com>
2022-12-21 13:55:10 +0300

    NodaJS|odev 6 eklendi

---
+++ js/koaServer.js
@@ -0,0 +1,20 @@
+import Koa from 'koa';
+const app = new Koa();
+
+// response
+app.use(ctx => {
+    const url = ctx.url;
+    console.log(url)
+    if(url==='/'){
+        ctx.body = '<h1>İndex sayfama hoşgeldiniz</h1>';
+    } else if(url==='/about'){ 
+        ctx.body = '<h1>hakkımda sayfama hoşgeldiniz</h1>';
+    } else if(url==='/contact'){
+        ctx.body = '<h1>hakkımda sayfama hoşgeldiniz</h1>';
+    }else{
+        ctx.response.status = 404;
+        ctx.body = '<h1>404: Sayfa Bulunamadı</h1>';
+    }
+});
+
+app.listen(3000);
